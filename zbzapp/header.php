53b07a8f77c9bc727a3621bc8eded52896cebd9a
bhavyanshu <bhavyanshu.spl@gmail.com>
2013-07-13 16:15:43 +0530

    Link to github added

--- zbzapp/header.php
+++ zbzapp/header.php
@@ -13,11 +13,8 @@ include('head.php');
                    <!--These are just samples, use your own icons. If you use larger ones, make sure too change the css-file to fit them in.
                        Dont´t forget to place your links -->
                     <div class="social">
-                    <a href="http://www.facebook.com/profile.php?id=100001520859552" title="facebook"><img src="img/facebook.png" width="20" height="20" alt="facebook"></a>
-                    <a href="http://twitter.com/#!/sg_layout" title="twitter"><img src="img/twitter.png" width="20" height="20" alt="twitter"></a>
-                    <a href="#" title="linkedin"><img src="img/linkedin.png" width="20" height="20" alt="linkedin"></a>
-                    <a href="#" title="vcard"><img src="img/vcard.png" width="20" height="20" alt="vcard"></a>
-                    <a href="#" title="picasa"><img src="img/picasa.png" width="20" height="20" alt="picasa"></a>
+                  <div class="social">
+                    <p>Get the source code from <a href="https://github.com/bhavyanshu/zBzQuiz" title="github"><img src="img/github.png" width="32" height="32" alt="Github"></a></p> 
                     </div>
                  
                     
